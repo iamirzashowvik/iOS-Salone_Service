fcccdf67e51e4d0eed56f07ebaa8fed54535925d
mirzashowvik <showvikmirza@gmail.com>
2023-05-20 12:10:41 +0600

    Initial Commit

---
+++ Salone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Salone
+//
+//  Created by Mirza  on 20/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Salone/SaloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaloneApp.swift
+//  Salone
+//
+//  Created by Mirza  on 20/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SaloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
